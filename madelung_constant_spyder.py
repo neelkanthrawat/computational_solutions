2d116625949390717334b8db2197c98f8e16a2e0
neelkanthrawat <42956384+neelkanthrawat@users.noreply.github.com>
2021-03-20 16:10:06 +0530

    Update madelung_constant_spyder.py

--- madelung_constant_spyder.py
+++ madelung_constant_spyder.py
@@ -4,7 +4,7 @@ Created on Sat Mar 20 08:53:09 2021
 
 @author: NEELKANTH RAWAT
 """
-#this program is to evaluate the madelung constant
+#this program is to evaluate the madelung constant of fcc and sc ionic crystals
 #not finished yet
 import numpy as np
 
@@ -30,26 +30,20 @@ def dist_block_matrix(shape_tuple):
 #extracting elements lying in the y-z plane(excluding the z axis) #their multiplicity is 8
 def yz_plane_excluding_z_axis(amat): 
     shape_of_amat=np.shape(amat)
-    #print("elements lying in the y-z plane(excluding the z axis)")
     yz_plane_exclud_z=amat[ 0:shape_of_amat[0],0,1:shape_of_amat[2] ]
-    #print(yz_plane_exclud_z)
     return yz_plane_exclud_z
 
 #extracting elements lying in the diagonal plane (excluding elements lying in the z axis) #their multiplicity is 8
 def diag_plane_excluding_z_axis(bmat):
     shape_of_bmat=np.shape(bmat)
-    #print("elements lying in the diagonal sheet excluding the z axis.")
     diag_plane_exclud_z=np.zeros((shape_of_bmat[1]-1,shape_of_bmat[0]))#initialising the matrix
     for rows in range(0,np.shape(diag_plane_exclud_z)[0],1):#no. of rows for diag_sheet_without_z_axis would be = no. of stacks
         diag_plane_exclud_z[rows,:]=bmat[:,rows+1,rows+1]
-    #print(diag_plane_exclud_z)
     return diag_plane_exclud_z
 
 #extracting elements lying in the z axis #their multiplicity is 2
 def z_axis_elements(cmat):   
     z_axis=cmat[:,0,0]
-    #print("elements lying in the z axis")
-    #print(z_axis)
     return z_axis
 
 #creating list of elements which have multiplicity of 16 
@@ -61,9 +55,6 @@ def elements_with_multiplicity_16(dmat):
         for i in range(2,shape_of_dmat[1],1):
             for j in range(1,i,1):
                 a.append(dmat[k,i,j])
-    #print("elements whose multiplicity is 16")
-    #print(a) 
-    #print("total number of elements whose multiplicity is 16 = ",len(a))
     return a
 
 #main program
@@ -84,7 +75,6 @@ print(yz_plane_ex_z)
 diag_without_z_plane=diag_plane_excluding_z_axis(dist_3d_mat)
 print("diagonal plane elements (excluding the z axis)")
 print(diag_without_z_plane)
-#(1) now i need to define some functions to make my code more clean and readable
-#(2) all i need now is to count the number of elements of given value and evaluate the series of madelung constant
+#(1) all i need to do now is to count the number of elements of given value and evaluate the series of madelung constant
 #np.count_nonzero(m==x)
 #listname.count(value)
